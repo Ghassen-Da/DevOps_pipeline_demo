1ef7110266dfc1af28a6337fea9f96a6c7320a73
Ghassen-Da <ghassendaoud99@gmail.com>
2022-04-22 15:35:46 +0100

    test fixed

--- index.spec.js
+++ index.spec.js
@@ -2,6 +2,6 @@ const {helloWorld}=require('./index.js')
 // hello world test
 describe('hello world', function () {
     it('should return hello world', function () {
-        expect(helloWorld()).toEqual('hello world!');
+        expect(helloWorld()).toEqual('hello world');
     });
 });
