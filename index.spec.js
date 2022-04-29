b08e9617d65d3d5d80b0198162803683a5a77047
Ghassen-Da <ghassendaoud99@gmail.com>
2022-04-29 13:58:43 +0100

    dockerize the application

--- index.spec.js
+++ index.spec.js
@@ -1,5 +1,5 @@
 const {helloWorld}=require('./index.js')
-// hello world test
+
 describe('hello world', function () {
     it('should return hello world', function () {
         expect(helloWorld()).toEqual('hello world');
